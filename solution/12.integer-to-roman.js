29b99f27b6be2e1703e87466ad52384feb416f02
wangyang <wangy40@mingyuanyun.com>
2019-06-30 15:37:41 +0800

    int-to-roman

--- solution/12.integer-to-roman.js
+++ solution/12.integer-to-roman.js
@@ -84,6 +84,31 @@
  * @return {string}
  */
 var intToRoman = function(num) {
-    
+  let nums = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+    dic = {
+      1000: "M",
+      900: "CM",
+      500: "D",
+      400: "CD",
+      100: "C",
+      90: "XC",
+      50: "L",
+      40: "XL",
+      10: "X",
+      9: "IX",
+      5: "V",
+      4: "IV",
+      1: "I"
+    },
+    str = '';
+  
+    nums.map(item => {
+      while (item<=num) {
+        num -= item;
+        str += dic[item]
+      }
+    })
+    return str;
 };
 
+intToRoman(1994)
