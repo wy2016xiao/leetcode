f275ff4a2b3b32c58967330e5e14824f12b6308a
wangyang <wangy40@mingyuanyun.com>
2019-07-21 16:57:02 +0800

    longest common presfix

---
+++ solution/22.generate-parentheses.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=22 lang=javascript
+ *
+ * [22] Generate Parentheses
+ *
+ * https://leetcode.com/problems/generate-parentheses/description/
+ *
+ * algorithms
+ * Medium (55.61%)
+ * Likes:    2962
+ * Dislikes: 188
+ * Total Accepted:    361.8K
+ * Total Submissions: 647.7K
+ * Testcase Example:  '3'
+ *
+ * 
+ * Given n pairs of parentheses, write a function to generate all combinations
+ * of well-formed parentheses.
+ * 
+ * 
+ * 
+ * For example, given n = 3, a solution set is:
+ * 
+ * 
+ * [
+ * ⁠ "((()))",
+ * ⁠ "(()())",
+ * ⁠ "(())()",
+ * ⁠ "()(())",
+ * ⁠ "()()()"
+ * ]
+ * 
+ */
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    
+};
+
